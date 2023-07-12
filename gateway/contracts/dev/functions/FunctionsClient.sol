03fb7162d029f3902c5dd795e2f2ba7cdd1dcbbf
jiang <jiang762507357@163.com>
2023-07-12 18:46:15 +0800

    add contract and code about chain

---
+++ gateway/contracts/dev/functions/FunctionsClient.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "./Functions.sol";
+import "../interfaces/FunctionsClientInterface.sol";
+import "../interfaces/FunctionsOracleInterface.sol";
+
+/**
+ * @title The Chainlink Functions client contract
+ * @notice Contract writers can inherit this contract in order to create Chainlink Functions requests
+ */
+abstract contract FunctionsClient is FunctionsClientInterface{
+  FunctionsOracleInterface internal s_oracle;
+  mapping(bytes32 => address) internal s_pendingRequests;
+
+  event RequestSent(bytes32 indexed id);
+  event RequestFulfilled(bytes32 indexed id);
+
+  event OracleRequest(
+    bytes32 indexed requestId,
+    address requestingContract,
+    address requestInitiator,
+    uint64 subscriptionId,
+    address subscriptionOwner,
+    bytes data
+  );
+
+  error SenderIsNotRegistry();
+  error EmptyRequestData();
+  error RequestIsAlreadyPending();
+
+
+
+
+  /**
+   * @notice Sends a Chainlink Functions request to the stored oracle address
+   * @param req The initialized Functions.Request
+   * @param subscriptionId The subscription ID
+   * @param gasLimit gas limit for the fulfillment callback
+   * @return requestId The generated request ID
+   */
+  function sendRequest(
+    Functions.Request memory req,
+    uint64 subscriptionId,
+    uint32 gasLimit
+  ) internal returns (bytes32) {
+
+    bytes32 requestId = s_oracle.sendRequest(subscriptionId, Functions.encodeCBOR(req), gasLimit);
+    s_pendingRequests[requestId] = address(0x01);
+    emit RequestSent(requestId);
+    return requestId;
+  }
+
+  
+  /**
+    * @notice User defined function to handle a response
+   * @param requestId The request ID, returned by sendRequest()
+   * @param response Aggregated response from the user code
+   * @param err Aggregated error from the user code or from the execution pipeline
+   * Either response or error parameter will be set, but never both
+   */
+  function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal virtual;
+
+  function handleOracleFulfillment(
+    bytes32 requestId,
+    bytes memory response,
+    bytes memory err
+  ) external override recordFulfillment(requestId) {
+    fulfillRequest(requestId, response, err);
+  }
+  /**
+   * @dev Reverts if the sender is not the oracle that serviced the request.
+   * Emits RequestFulfilled event.
+   * @param requestId The request ID for fulfillment
+   */
+  modifier recordFulfillment(bytes32 requestId) {
+    if (msg.sender != s_pendingRequests[requestId]) {
+      revert SenderIsNotRegistry();
+    }
+    delete s_pendingRequests[requestId];
+    emit RequestFulfilled(requestId);
+    _;
+  }
+  /**
+   * @dev Reverts if the request is already pending
+   * @param requestId The request ID for fulfillment
+   */
+  modifier notPendingRequest(bytes32 requestId) {
+    if (s_pendingRequests[requestId] != address(0)) {
+      revert RequestIsAlreadyPending();
+    }
+    _;
+  }
+}
