3ba188a3e3db5b4e3dcddb93f7b11066520a7f92
jiang <jiang762507357@163.com>
2023-08-01 23:34:04 +0800

    develop contract about function

---
+++ gateway/contracts/BaseManagerImpl.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./registry.sol";
+import "./baseManager.sol";
+
+contract BaseManagerImpl is ERC721, BaseManager, Ownable{
+ // A map of expiry times
+    mapping(uint256 => uint256) expiries;
+    // The funtion registry
+    Registry public reg;
+    // The namehash of the TLD this registrar owns (eg, .eth)
+    bytes32 public baseNode;
+    // A map of addresses that are authorised to register and renew names.
+    mapping(address => bool) public controllers;
+
+    bytes functionMetaData;
+
+    /**
+     * v2.1.3 version of _isApprovedOrOwner which calls ownerOf(tokenId) and takes grace period into consideration instead of ERC721.ownerOf(tokenId);
+     * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.1.3/contracts/token/ERC721/ERC721.sol#L187
+     * @dev Returns whether the given spender can transfer a given token ID
+     * @param spender address of the spender to query
+     * @param tokenId uint256 ID of the token to be transferred
+     * @return bool whether the msg.sender is approved for the given token ID,
+     *    is an operator of the owner, or is the owner of the token
+     */
+    function _isApprovedOrOwner(
+        address spender,
+        uint256 tokenId
+    ) internal view override returns (bool) {
+        address owner = ownerOf(tokenId);
+        return (spender == owner ||
+            getApproved(tokenId) == spender ||
+            isApprovedForAll(owner, spender));
+    }
+
+    constructor(Registry _reg, bytes32 _baseNode) ERC721("", "") {
+        reg = _reg;
+        baseNode = _baseNode;
+    }
+
+    modifier live() {
+        require(reg.owner(baseNode) == address(this));
+        _;
+    }
+
+    modifier onlyController() {
+        require(controllers[msg.sender]);
+        _;
+    }
+
+    /**
+     * @dev Gets the owner of the specified token ID. Names become unowned
+     *      when their registration expires.
+     * @param tokenId uint256 ID of the token to query the owner of
+     * @return address currently marked as the owner of the given token ID
+     */
+    function ownerOf(
+        uint256 tokenId
+    ) public view override(IERC721, ERC721) returns (address) {
+        require(expiries[tokenId] > block.timestamp);
+        return super.ownerOf(tokenId);
+    }
+
+    // Authorises a controller, who can register and renew domains.
+    function addController(address controller) external override onlyOwner {
+        controllers[controller] = true;
+        emit ControllerAdded(controller);
+    }
+
+    // Revoke controller permission for an address.
+    function removeController(address controller) external override onlyOwner {
+        controllers[controller] = false;
+        emit ControllerRemoved(controller);
+    }
+
+    // Set the resolver for the TLD this registrar manages.
+    function setManager(address resolver) external override onlyOwner {
+        reg.setManager(baseNode, resolver);
+    }
+
+
+    /**
+     * @dev Register a name.
+     * @param id The token ID (keccak256 of the label).
+     * @param owner The address that should own the registration.
+     * @param duration Duration in seconds for the registration.
+     */
+    function register(
+        uint256 id,
+        address owner,
+        uint256 duration
+    ) external override returns (uint256) {
+        return _register(id, owner, duration, true);
+    }
+
+    /**
+     * @dev Register a name, without modifying the registry.
+     * @param id The token ID (keccak256 of the label).
+     * @param owner The address that should own the registration.
+     * @param duration Duration in seconds for the registration.
+     */
+    function registerOnly(
+        uint256 id,
+        address owner,
+        uint256 duration
+    ) external returns (uint256) {
+        return _register(id, owner, duration, false);
+    }
+
+    function _register(
+        uint256 id,
+        address owner,
+        uint256 duration,
+        bool updateRegistry
+    ) internal live onlyController returns (uint256) {
+       
+        _mint(owner, id);
+        if (updateRegistry) {
+            reg.setSubnodeOwner(baseNode, bytes32(id), owner);
+        }
+
+        emit NameRegistered(id, owner, block.timestamp + duration);
+
+        return block.timestamp + duration;
+    }
+
+    /**
+     * @dev Reclaim ownership of a name in ENS, if you own it in the registrar.
+     */
+    function reclaim(uint256 id, address owner) external override live {
+        require(_isApprovedOrOwner(msg.sender, id));
+        reg.setSubnodeOwner(baseNode, bytes32(id), owner);
+    }
+
+    function updateMetaData(bytes memory funcBytes) external {
+        functionMetaData = funcBytes;
+        emit MetaDataUpdated(funcBytes);
+        return;
+    }
+
+  
+}
--- gateway/contracts/baseManager.sol
+++ gateway/contracts/baseManager.sol
@@ -2,27 +2,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./registry.sol";
 
-contract BaseManager is IERC721, Ownable {
-    uint constant public GRACE_PERIOD = 90 days;
-
+interface BaseManager is IERC721 {
     event ControllerAdded(address indexed controller);
     event ControllerRemoved(address indexed controller);
-    event NameMigrated(uint256 indexed id, address indexed owner, uint expires);
-    event NameRegistered(uint256 indexed id, address indexed owner, uint expires);
-    event NameRenewed(uint256 indexed id, uint expires);
+    event NameMigrated(
+        uint256 indexed id,
+        address indexed owner,
+        uint256 expires
+    );
+    event NameRegistered(
+        uint256 indexed id,
+        address indexed owner,
+        uint256 expires
+    );
     event MetaDataUpdated(bytes funcMeta);
-    // The ENS registry
-    ENS public ens;
-
-    // The namehash of the TLD this registrar owns (eg, .eth)
-    bytes32 public baseNode;
-
-    // A map of addresses that are authorised to register and renew names.
-    mapping(address=>bool) public controllers;
 
     // Authorises a controller, who can register and renew domains.
     function addController(address controller) external;
@@ -31,15 +27,17 @@ contract BaseManager is IERC721, Ownable {
     function removeController(address controller) external;
 
     // Set the resolver for the TLD this registrar manages.
-    function setResolver(address resolver) external;
+    function setManager(address resolver) external;
 
-    // Returns true iff the specified name is available for registration.
-    function available(uint256 id) public view returns(bool);
 
     /**
      * @dev Register a name.
      */
-    function register(uint256 id, address owner, uint duration) external returns(uint);
+    function register(
+        uint256 id,
+        address owner,
+        uint256 duration
+    ) external returns (uint256);
 
     /**
      * @dev Reclaim ownership of a name in ENS, if you own it in the registrar.
@@ -48,3 +46,47 @@ contract BaseManager is IERC721, Ownable {
 
     function updateMetaData(bytes memory func) external;
 }
+
+
+// interface BaseManager is  Ownable {
+//     uint constant public GRACE_PERIOD = 90 days;
+
+//     event ControllerAdded(address indexed controller);
+//     event ControllerRemoved(address indexed controller);
+//     event NameMigrated(uint256 indexed id, address indexed owner, uint expires);
+//     event NameRegistered(uint256 indexed id, address indexed owner, uint expires);
+//     event NameRenewed(uint256 indexed id, uint expires);
+//     event MetaDataUpdated(bytes funcMeta);
+//     // The function registry
+//     Registry public reg;
+
+//     // The namehash of the TLD this registrar owns (eg, .eth)
+//     bytes32 public baseNode;
+
+//     // A map of addresses that are authorised to register and renew names.
+//     mapping(address=>bool) public controllers;
+
+//     // Authorises a controller, who can register and renew domains.
+//     function addController(address controller) external;
+
+//     // Revoke controller permission for an address.
+//     function removeController(address controller) external;
+
+//     // Set the resolver for the TLD this registrar manages.
+//     function setManager(address resolver) external;
+
+//     // Returns true iff the specified name is available for registration.
+//     function available(uint256 id) public view returns(bool);
+
+//     /**
+//      * @dev Register a name.
+//      */
+//     function register(uint256 id, address owner, uint duration) external returns(uint);
+
+//     /**
+//      * @dev Reclaim ownership of a name in ENS, if you own it in the registrar.
+//      */
+//     function reclaim(uint256 id, address owner) external;
+
+//     function updateMetaData(bytes memory func) external;
+// }
